c95e1c96b8276b0c8e18a202cc6ca068877e9013
Jack Westmore <jackwestmore@gmail.com>
2023-11-01 19:24:24 +0000

    fix positioning of footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,6 @@
 export default async function Footer() {
   return (
-    <div className='fixed bottom-0 flex w-full flex-col items-center justify-center'>
+    <div className='static bottom-0 flex w-full flex-col items-center justify-center'>
       <p className='my-3 text-sm font-light text-gray-700'>
         Copyright © 2023 Supersonic Industries
       </p>
