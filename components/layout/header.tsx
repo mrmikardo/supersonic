d6278e09a80cd421813f30725d5e040fb5c6c7b6
Jack Westmore <jackwestmore@gmail.com>
2023-11-01 19:24:06 +0000

    components: header: wrap logo in a Link

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,12 +1,15 @@
 import Cart from '@/components/icons/cart';
 import Instagram from '@/components/icons/instagram';
 import Logo from '@/components/logo';
+import Link from 'next/link';
 
 export default async function Header() {
   return (
     <div className='flex w-full justify-between border-b border-orange-500'>
-      {/* @ts-expect-error Async Server Component */}
-      <Logo />
+      <Link href='/'>
+        {/* @ts-expect-error Async Server Component */}
+        <Logo />
+      </Link>
       <div className='flex items-center justify-center gap-8 px-12 text-gray-700'>
         <Instagram />
         <Cart />
