42c2ae036c213e67620e1868c4d35c525b3ca0fb
Jack Westmore <jackwestmore@gmail.com>
2023-11-01 19:18:29 +0000

    routes: about: + placeholder text, styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,28 @@
 export default async function About() {
-  return <p>This is the about page.</p>;
+  return (
+    <div className='flex w-full flex-col items-center justify-center'>
+      <div className='flex w-1/3 flex-col gap-2 self-center text-justify'>
+        <p>
+          Bacon ipsum dolor amet doner shoulder corned beef, pig sausage
+          turducken kevin swine. Ribeye strip steak andouille tri-tip landjaeger
+          bacon swine chuck picanha pork chop short loin biltong prosciutto
+          pancetta. Flank porchetta turducken, meatloaf pastrami spare ribs
+          buffalo short loin hamburger pancetta ribeye tenderloin boudin.
+        </p>
+        <br />
+        <p>
+          Spare ribs venison prosciutto, tri-tip bresaola picanha buffalo jowl
+          ham filet mignon biltong. Pig short ribs salami pork loin. Ball tip
+          jowl drumstick shoulder meatloaf turkey.
+        </p>
+        <br />
+        <p>
+          Chicken kielbasa pig, ham landjaeger meatball short ribs shoulder
+          t-bone bresaola tri-tip tail short loin picanha. Pork chop short loin
+          strip steak buffalo. Buffalo filet mignon porchetta fatback strip
+          steak, andouille boudin leberkas meatball jowl shankle.
+        </p>
+      </div>
+    </div>
+  );
 }
