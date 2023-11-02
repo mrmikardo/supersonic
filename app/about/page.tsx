4a6ded737e52b1d0f5cca1158f2393d66dff7760
Jack Westmore <jackwestmore@gmail.com>
2023-11-02 21:44:08 +0000

    formatting

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export const runtime = 'edge';
 export default async function About() {
   return (
     <div className='flex w-full flex-col items-center justify-center'>
-      <div className='flex flex-col gap-2 self-center px-4 text-justify md:w-1/3'>
+      <div className='flex flex-col gap-2 self-center px-4 text-justify md:w-3/4 lg:w-1/3'>
         <p>
           David, Jack and Tristan founded Supersonic in 2023. All three share a
           love for well-crafted, beautiful clothing which brings joy to its
