666ba04bedef5696541a953451c21d46fbf83320
Jack Westmore <jackwestmore@gmail.com>
2023-11-02 13:50:34 +0000

    initial product (cart) work

---
+++ components/product/product-description.tsx
@@ -0,0 +1,34 @@
+import { AddToCart } from '@/components/cart/add-to-cart';
+import Price from '@/components/price';
+import Prose from '@/components/prose';
+import { Product } from '@/lib/shopify/types';
+import { VariantSelector } from './variant-selector';
+
+export function ProductDescription({ product }: { product: Product }) {
+  return (
+    <>
+      <div className='mb-6 flex flex-col border-b pb-6 dark:border-neutral-700'>
+        <h1 className='mb-2 text-4xl'>{product.title}</h1>
+        <div className='mr-auto w-auto rounded-full border border-orange-500 p-2 text-sm text-orange-500'>
+          <Price
+            amount={product.priceRange.maxVariantPrice.amount}
+            currencyCode={product.priceRange.maxVariantPrice.currencyCode}
+          />
+        </div>
+      </div>
+      <VariantSelector options={product.options} variants={product.variants} />
+
+      {product.descriptionHtml ? (
+        <Prose
+          className='mb-6 text-sm leading-tight'
+          html={product.descriptionHtml}
+        />
+      ) : null}
+
+      <AddToCart
+        variants={product.variants}
+        availableForSale={product.availableForSale}
+      />
+    </>
+  );
+}
