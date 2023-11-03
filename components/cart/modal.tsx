229c0ed0cfb5bce1f5744f0074e0b5d6f8674b15
Jack Westmore <jackwestmore@gmail.com>
2023-11-03 14:06:54 +0000

    components: improve cart aesthetics

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -103,7 +103,7 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                       return (
                         <li
                           key={i}
-                          className='flex w-full flex-col border-b border-neutral-300 dark:border-neutral-700'
+                          className='flex w-full flex-col border-b border-neutral-300 border-neutral-500'
                         >
                           <div className='relative flex w-full flex-row justify-between px-1 py-4'>
                             <div className='absolute z-40 -mt-2 ml-[55px]'>
@@ -114,7 +114,7 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                               onClick={closeCart}
                               className='z-30 flex flex-row space-x-4'
                             >
-                              <div className='relative h-16 w-16 cursor-pointer overflow-hidden rounded-md border border-neutral-300 bg-neutral-300 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:bg-neutral-800'>
+                              <div className='relative h-16 w-16 cursor-pointer overflow-hidden rounded-md border border-neutral-300  border-neutral-500 bg-neutral-300 hover:bg-neutral-300'>
                                 <Image
                                   className='h-full w-full object-cover'
                                   width={64}
@@ -148,7 +148,7 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                                   item.cost.totalAmount.currencyCode
                                 }
                               />
-                              <div className='ml-auto flex h-9 flex-row items-center rounded-full border border-neutral-200 dark:border-neutral-700'>
+                              <div className='ml-auto flex h-9 flex-row items-center rounded-full border border-neutral-500'>
                                 <EditItemQuantityButton
                                   item={item}
                                   type='minus'
@@ -169,23 +169,23 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                       );
                     })}
                   </ul>
-                  <div className='py-4 text-sm text-neutral-500 dark:text-neutral-400'>
-                    <div className='mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 dark:border-neutral-700'>
+                  <div className='py-4 text-sm text-neutral-500'>
+                    <div className='mb-3 flex items-center justify-between border-b border-neutral-500 pb-1'>
                       <p>Taxes</p>
                       <Price
-                        className='text-right text-base text-black dark:text-white'
+                        className='text-right text-base text-black'
                         amount={cart.cost.totalTaxAmount.amount}
                         currencyCode={cart.cost.totalTaxAmount.currencyCode}
                       />
                     </div>
-                    <div className='mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 dark:border-neutral-700'>
+                    <div className='mb-3 flex items-center justify-between border-b  pb-1 pt-1 border-neutral-500'>
                       <p>Shipping</p>
                       <p className='text-right'>Calculated at checkout</p>
                     </div>
-                    <div className='mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 dark:border-neutral-700'>
+                    <div className='mb-3 flex items-center justify-between border-b  pb-1 pt-1 border-neutral-500'>
                       <p>Total</p>
                       <Price
-                        className='text-right text-base text-black dark:text-white'
+                        className='text-right text-base text-black'
                         amount={cart.cost.totalAmount.amount}
                         currencyCode={cart.cost.totalAmount.currencyCode}
                       />
@@ -193,7 +193,7 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                   </div>
                   <a
                     href={cart.checkoutUrl}
-                    className='block w-full rounded-full bg-blue-600 p-3 text-center text-sm font-medium text-white opacity-90 hover:opacity-100'
+                    className='block w-full rounded-full bg-orange-600 p-3 text-center text-sm font-medium text-white opacity-90 hover:opacity-100'
                   >
                     Proceed to Checkout
                   </a>
--- components/cart/open-cart.tsx
+++ components/cart/open-cart.tsx
@@ -18,7 +18,7 @@ export default function OpenCart({
       />
 
       {quantity ? (
-        <div className='absolute right-0 top-0 -mr-2 -mt-2 h-4 w-4 rounded bg-orange-700 text-[11px] font-medium text-white'>
+        <div className='absolute right-0 top-0 mt-8 mr-10 h-4 w-4 rounded bg-orange-700 text-[11px] font-medium text-white'>
           {quantity}
         </div>
       ) : null}
